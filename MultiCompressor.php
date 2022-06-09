e5debacb0d219d937f7e3a49df29167f9ed50ec0
larryTheCoder <larryPeter132@gmail.com>
2022-06-10 01:11:41 +0800

    Revert "Lets debug this the stone-age way"
    This reverts commit 0e53406c2e3401240c98e686e890b2e6aab91cc4.

--- MultiCompressor.php
+++ MultiCompressor.php
@@ -6,9 +6,6 @@ declare(strict_types=1);
 namespace libproxy;
 
 
-use Akeeba\Engine\Postproc\Connector\S3v4\Configuration;
-use Akeeba\Engine\Postproc\Connector\S3v4\Connector;
-use Akeeba\Engine\Postproc\Connector\S3v4\Input;
 use ErrorException;
 use GlobalLogger;
 use pocketmine\network\mcpe\compression\Compressor;
@@ -17,8 +14,7 @@ use pocketmine\network\mcpe\compression\ZlibCompressor;
 use pocketmine\utils\BinaryDataException;
 use pocketmine\utils\BinaryStream;
 use pocketmine\utils\SingletonTrait;
-use Ramsey\Uuid\Uuid;
-use Throwable;
+use RuntimeException;
 use function zstd_uncompress;
 
 class MultiCompressor implements Compressor
@@ -29,18 +25,6 @@ class MultiCompressor implements Compressor
     public const METHOD_ZSTD = 0x01;
 
     use SingletonTrait;
-
-    /** @var string */
-    public static string $certPath = '';
-    /** @var string */
-    private string $cacert;
-
-    public function __construct()
-    {
-        $this->cacert = self::$certPath;
-
-        var_dump($this->cacert);
-    }
 
     public function willCompress(string $data): bool
     {
@@ -84,22 +68,8 @@ class MultiCompressor implements Compressor
     public function compress(string $payload): string
     {
         if (($size = strlen($payload)) >= (3.5 * 1024 * 1024)) {
-            try {
-                // constants required by akeeba/s3 lib to work
-                defined("AKEEBAENGINE") or define("AKEEBAENGINE", 1);
-                defined("AKEEBA_CACERT_PEM") or define("AKEEBA_CACERT_PEM", $this->cacert);
-
-                $configuration = new Configuration('7KW1MAYJ2J3W6A9YUFXJ', 'zUPjyDcciTGbRRdHyYy7le7czUcgRucgO3McMbz4', "v4", 'eu-central-1');
-                $configuration->setEndpoint('s3.eu-central-1.wasabisys.com');
-
-                $uuid = Uuid::uuid4();
-                $connector = new Connector($configuration);
-                $connector->putObject(Input::createFromData($payload), "ng-test-bucket", 'packet_dumps/' . $uuid->toString() . '.raw');
-
-                GlobalLogger::get()->alert("Packet dumped into " . $uuid->toString() . ' for exceeding limit: ' . $size);
-            } catch (Throwable) {
-                GlobalLogger::get()->alert("Something went wrong when trying to upload a packet into s3, $size ");
-            }
+            GlobalLogger::get()->alert("Payload exceed maximum safe decompression size, $size.");
+            GlobalLogger::get()->logException(new RuntimeException());
         }
 
         return ZlibCompressor::getInstance()->compress($payload);
