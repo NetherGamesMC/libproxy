f53145ee32c3770bb83c71742393f417b0205b03
Dries C <coenendries3@gmail.com>
2024-03-05 13:27:20 +0100

    Handle ping pong on thread

--- ProxyServer.php
+++ ProxyServer.php
@@ -12,19 +12,36 @@ use libproxy\protocol\ProxyPacket;
 use libproxy\protocol\ProxyPacketPool;
 use libproxy\protocol\ProxyPacketSerializer;
 use pmmp\thread\ThreadSafeArray;
+use pocketmine\network\mcpe\compression\DecompressionException;
+use pocketmine\network\mcpe\compression\ZlibCompressor;
+use pocketmine\network\mcpe\NetworkSession;
+use pocketmine\network\mcpe\PacketRateLimiter;
+use pocketmine\network\mcpe\protocol\NetworkStackLatencyPacket;
+use pocketmine\network\mcpe\protocol\Packet as BedrockPacket;
+use pocketmine\network\mcpe\protocol\PacketDecodeException;
+use pocketmine\network\mcpe\protocol\PacketPool;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
+use pocketmine\network\mcpe\protocol\RequestNetworkSettingsPacket;
+use pocketmine\network\mcpe\protocol\serializer\PacketBatch;
+use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
+use pocketmine\network\mcpe\protocol\types\CompressionAlgorithm;
 use pocketmine\network\mcpe\raklib\PthreadsChannelReader;
 use pocketmine\network\mcpe\raklib\SnoozeAwarePthreadsChannelWriter;
 use pocketmine\network\PacketHandlingException;
 use pocketmine\snooze\SleeperHandlerEntry;
-use pocketmine\snooze\SleeperNotifier;
 use pocketmine\thread\log\AttachableThreadSafeLogger;
 use pocketmine\utils\Binary;
 use pocketmine\utils\BinaryDataException;
+use pocketmine\utils\BinaryStream;
 use pocketmine\utils\Utils;
 use raklib\generic\SocketException;
 use Socket;
+use function base64_encode;
+use function bin2hex;
+use function chr;
 use function count;
 use function min;
+use function ord;
 use function socket_accept;
 use function socket_close;
 use function socket_getpeername;
@@ -37,7 +54,9 @@ use function socket_shutdown;
 use function socket_strerror;
 use function socket_write;
 use function strlen;
+use function substr;
 use function trim;
+use function zstd_uncompress;
 use const MSG_DONTWAIT;
 use const MSG_WAITALL;
 use const SO_LINGER;
@@ -70,6 +89,14 @@ class ProxyServer
 
     /** @var Socket[] */
     private array $sockets = [];
+
+    /** @phpstan-var array<int, PacketRateLimiter> */
+    private array $gamePacketLimiter = [];
+    /** @phpstan-var array<int, PacketRateLimiter> */
+    private array $batchPacketLimiter = [];
+    /** @phpstan-var array<int, int> */
+    private array $protocolId = [];
+
     /** @phpstan-var array<int, array<int|string>> */
     private array $socketBuffer = [];
 
@@ -109,7 +136,13 @@ class ProxyServer
                 $this->logger->debug('Socket is not connected anymore.');
             }
             socket_close($socket);
-            unset($this->sockets[$socketId], $this->socketBuffer[$socketId]);
+            unset(
+                $this->sockets[$socketId],
+                $this->socketBuffer[$socketId],
+                $this->gamePacketLimiter[$socketId],
+                $this->batchPacketLimiter[$socketId],
+                $this->protocolId[$socketId]
+            );
         }
 
         $this->logger->debug("Disconnected socket with id " . $socketId);
@@ -125,6 +158,16 @@ class ProxyServer
     public function getSocket(int $socketId): ?Socket
     {
         return $this->sockets[$socketId] ?? null;
+    }
+
+    private function getGamePacketLimiter(int $streamIdentifier): PacketRateLimiter
+    {
+        return $this->gamePacketLimiter[$streamIdentifier] ??= new PacketRateLimiter("Game Packets", 2, 100);
+    }
+
+    private function getBatchPacketLimiter(int $streamIdentifier): PacketRateLimiter
+    {
+        return $this->batchPacketLimiter[$streamIdentifier] ??= new PacketRateLimiter("Batch Packets", 2, 100);
     }
 
     private function putPacket(int $socketId, ProxyPacket $pk): void
@@ -190,22 +233,166 @@ class ProxyServer
                         break;
                     case ForwardPacket::NETWORK_ID:
                         /** @var ForwardPacket $pk */
-                        if (($socket = $this->getSocket($socketId)) === null) {
-                            throw new PacketHandlingException('Socket with id (' . $socketId . ") doesn't exist.");
-                        }
-
-                        try {
-                            if (socket_write($socket, Binary::writeInt(strlen($pk->payload)) . $pk->payload) === false) {
-                                throw new PacketHandlingException('client disconnect');
-                            }
-                        } catch (ErrorException $exception) {
-                            throw PacketHandlingException::wrap($exception, 'client disconnect');
-                        }
+                        $this->sendPayload($socketId, $pk->payload);
                         break;
                 }
             } catch (PacketHandlingException $exception) {
                 $this->closeSocket($socketId, $exception->getMessage());
             }
+        }
+    }
+
+    /**
+     * Sends a payload to the client
+     */
+    private function sendPayload(int $socketId, string $payload): void
+    {
+        if (($socket = $this->getSocket($socketId)) === null) {
+            throw new PacketHandlingException('Socket with id (' . $socketId . ") doesn't exist.");
+        }
+
+        try {
+            if (socket_write($socket, Binary::writeInt(strlen($payload)) . $payload) === false) {
+                throw new PacketHandlingException('client disconnect');
+            }
+        } catch (ErrorException $exception) {
+            throw PacketHandlingException::wrap($exception, 'client disconnect');
+        }
+    }
+
+    /**
+     * Sends a data packet to the main thread.
+     */
+    private function sendDataPacketToMain(int $socketId, string $payload): void
+    {
+        $pk = new ForwardPacket();
+        $pk->payload = $payload;
+
+        $this->putPacket($socketId, $pk);
+    }
+
+    /**
+     * Returns the protocol ID for the given socket identifier.
+     */
+    private function getProtocolId(int $socketId): int
+    {
+        return $this->protocolId[$socketId] ?? ProtocolInfo::CURRENT_PROTOCOL;
+    }
+
+    /**
+     * Sends a data packet to the client using a single packet in a batch.
+     */
+    private function sendDataPacket(int $socketId, BedrockPacket $packet): void
+    {
+        $packetSerializer = PacketSerializer::encoder($protocolId = $this->getProtocolId($socketId));
+        $packet->encode($packetSerializer);
+
+        $stream = new BinaryStream();
+        PacketBatch::encodeRaw($stream, [$packetSerializer->getBuffer()]);
+        $payload = ($protocolId >= ProtocolInfo::PROTOCOL_1_20_60 ? chr(CompressionAlgorithm::ZLIB) : '') . ZlibCompressor::getInstance()->compress($stream->getBuffer());
+
+        $this->sendPayload($socketId, $payload);
+    }
+
+    private function decodePacket(int $socketId, BedrockPacket $packet, string $buffer): void
+    {
+        $stream = PacketSerializer::decoder($this->protocolId[$socketId] ?? ProtocolInfo::CURRENT_PROTOCOL, $buffer, 0);
+        try {
+            $packet->decode($stream);
+        } catch (PacketDecodeException $e) {
+            throw PacketHandlingException::wrap($e);
+        }
+        if (!$stream->feof()) {
+            $remains = substr($stream->getBuffer(), $stream->getOffset());
+            $this->logger->debug("Still " . strlen($remains) . " bytes unread in " . $packet->getName() . ": " . bin2hex($remains));
+        }
+    }
+
+    /**
+     * Returns true if the packet was handled successfully, false if it should be sent to the main thread.
+     *
+     * @return bool whether the packet was handled successfully
+     */
+    private function handleDataPacket(int $socketId, BedrockPacket $packet, string $buffer): bool
+    {
+        if ($packet->pid() == NetworkStackLatencyPacket::NETWORK_ID) {
+            /** @var NetworkStackLatencyPacket $packet USED FOR PING CALCULATIONS */
+            $this->decodePacket($socketId, $packet, $buffer);
+
+            if ($packet->timestamp === 0 && $packet->needResponse) {
+                try {
+                    $this->sendDataPacket($socketId, NetworkStackLatencyPacket::response(0));
+                } catch (PacketHandlingException $e) {
+                    // ignore, the client probably disconnected
+                }
+                return true;
+            }
+        } else if ($packet->pid() === RequestNetworkSettingsPacket::NETWORK_ID) {
+            /** @var RequestNetworkSettingsPacket $packet USED TO GET PROTOCOLID */
+            $this->decodePacket($socketId, $packet, $buffer);
+
+            $this->protocolId[$socketId] = $packet->getProtocolVersion();
+        }
+
+        return false;
+    }
+
+    /**
+     * @see NetworkSession::handleEncoded($payload)
+     */
+    private function onFullDataReceive(int $socketId, string $payload): void
+    {
+        try {
+            $this->getBatchPacketLimiter($socketId)->decrement();
+
+            if (strlen($payload) < 1) {
+                throw new PacketHandlingException("No bytes in payload");
+            }
+
+            $compressionType = ord($payload[0]);
+            $compressed = substr($payload, 1);
+
+            try {
+                $decompressed = match ($compressionType) {
+                    CompressionAlgorithm::NONE => $compressed,
+                    CompressionAlgorithm::ZLIB => ZlibCompressor::getInstance()->decompress($compressed),
+                    CompressionAlgorithm::NONE - 1 => ($d = zstd_uncompress($compressed)) === false ? throw new DecompressionException("Failed to decompress packet") : $d,
+                    default => throw new PacketHandlingException("Packet compressed with unexpected compression type $compressionType")
+                };
+            } catch (ErrorException|DecompressionException $e) {
+                $this->logger->debug("Failed to decompress packet: " . base64_encode($compressed));
+                throw PacketHandlingException::wrap($e, "Compressed packet batch decode error");
+            }
+
+            try {
+                $stream = new BinaryStream($decompressed);
+                $count = 0;
+                foreach (PacketBatch::decodeRaw($stream) as $buffer) {
+                    $this->getGamePacketLimiter($socketId)->decrement();
+                    if (++$count > 100) {
+                        throw new PacketHandlingException("Too many packets in batch");
+                    }
+                    $packet = PacketPool::getInstance()->getPacket($buffer);
+                    if ($packet === null) {
+                        $this->logger->debug("Unknown packet: " . base64_encode($buffer));
+                        throw new PacketHandlingException("Unknown packet received");
+                    }
+                    try {
+                        if (!$this->handleDataPacket($socketId, $packet, $buffer)) {
+                            $this->sendDataPacketToMain($socketId, $buffer);
+                        }
+                    } catch (PacketHandlingException $e) {
+                        $this->logger->debug($packet->getName() . ": " . base64_encode($buffer));
+                        throw PacketHandlingException::wrap($e, "Error processing " . $packet->getName());
+                    }
+                }
+            } catch (PacketDecodeException|BinaryDataException $e) {
+                $this->logger->logException($e);
+                throw PacketHandlingException::wrap($e, "Packet batch decode error");
+            }
+        } catch (PacketHandlingException $e) {
+            $this->logger->logException($e);
+            $this->closeSocket($socketId, $e->getMessage());
         }
     }
 
@@ -273,11 +460,7 @@ class ProxyServer
         // A null frame data indicates that there is not enough bytes to read.
         if ($rawFrameData !== null) {
             unset($this->socketBuffer[$socketId]);
-
-            $pk = new ForwardPacket();
-            $pk->payload = $rawFrameData;
-
-            $this->putPacket($socketId, $pk);
+            $this->onFullDataReceive($socketId, $rawFrameData);
         }
     }
 
